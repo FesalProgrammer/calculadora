fec8c6049ce8bc1c25a73ac35f231dab02c85551
FesalProgrammer <FesalProgramming@gmail.com>
2026-03-26 16:38:39 -0300

    Primer commit

---
+++ calculadora_code.py
@@ -0,0 +1,12 @@
+#Funciones con opeariones basicas
+
+def sumar(a, b):
+    return a + b 
+def restar(a, b):
+    return a - b 
+def multiplicar(a, b):
+    return a * b 
+def dividir(a, b):
+    if b == 0: 
+        raise ValueError("No se puede dividir por cero.") 
+    return a / b
